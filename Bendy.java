410a6171c7f97ceb8f2044f79512999f11925e9f
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2020-01-27 17:14:33 -0800

    Update Bendy.java

--- Bendy.java
+++ Bendy.java
@@ -9,8 +9,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @Disabled
 public class Bendy {
 	private DcMotor[] motors = new DcMotor[7];
-	private Gamepad gamepad3;
-	private Gamepad gamepad4;
+	private double[] velocity = {0,0,0,0,0,0,0};
+	private String[] names = {"a", "b", "x", "y"};
+	private boolean[] prevButtons = {false, false, false, false};
 	Bendy(HardwareMap hardwareMap) {
 		motors[0] = hardwareMap.get(DcMotor.class, "leftFront");
 		motors[1] = hardwareMap.get(DcMotor.class, "rightFront");
@@ -20,65 +21,47 @@ public class Bendy {
 		motors[5] = hardwareMap.get(DcMotor.class, "lift2");
 		motors[6] = hardwareMap.get(DcMotor.class, "slide");
 		
-		
 		for(int i = 0; i < motors.length; i++) {
-		motors[i].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-		if(i == 1 || i == 2) motors[i].setDirection(DcMotor.Direction.REVERSE);
-		motors[i].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-		motors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+			motors[i].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+			if(i == 1 || i == 2) motors[i].setDirection(DcMotor.Direction.REVERSE);
+			motors[i].setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+			motors[i].setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 		}
 	}
 	
+	private void setVelocity(int index, double newVelocity, double delay) {
+		velocity[index] = Math.sinh(velocity[index]) / delay + newVelocity * (1 - Math.sinh(1) / delay);
+		if(velocity[index] > 1) velocity[index] = 1;
+		else if(velocity[index] < -1) velocity[index] = -1;
+		motors[index].setPower(velocity[index]);
+	}
+	
 	public void drive(double angle, double power) {
-		motors[0].setPower(power * Math.sin(Math.toRadians(angle)));
-		motors[1].setPower(power * Math.cos(Math.toRadians(angle)));
-		motors[2].setPower(power * Math.sin(Math.toRadians(angle)));
-		motors[3].setPower(power * Math.cos(Math.toRadians(angle)));
+		setVelocity(0, power * Math.sin(Math.toRadians(angle)), 10);
+		setVelocity(1, power * Math.cos(Math.toRadians(angle)), 10);
+		setVelocity(2, power * Math.sin(Math.toRadians(angle)), 10);
+		setVelocity(3, power * Math.cos(Math.toRadians(angle)), 10);
 	}
 	
 	public void drive(double y, double x, double turn) {
 		double speed = Math.hypot(y, x);
 		double robotAngle = Math.atan2(y, x) - Math.PI / 4;
 
-		motors[0].setPower(speed * Math.sin(robotAngle) - turn);
-		motors[1].setPower(speed * Math.cos(robotAngle) + turn);
-		motors[2].setPower(speed * Math.sin(robotAngle) + turn);
-		motors[3].setPower(speed * Math.cos(robotAngle) - turn);
+		setVelocity(0, speed * Math.sin(robotAngle) - turn, 10);
+		setVelocity(1, speed * Math.cos(robotAngle) + turn, 10);
+		setVelocity(2, speed * Math.sin(robotAngle) + turn, 10);
+		setVelocity(3, speed * Math.cos(robotAngle) - turn, 10);
 	}
 	
-	public Gamepad getGamepad(int i) {
-		return (i == 0) ? gamepad3 : gamepad4;
-	}
-	
-	public boolean buttonToggle(boolean a, boolean b, Telemetry telemetry) {
-		telemetry.addData("a",a);
-		telemetry.addData("b", b);
-		telemetry.update();
-		return a && !b;
-	}
-	
-	public void updateControllers(Gamepad gamepad1, Gamepad gamepad2) {
-		gamepad3 = gamepad1;
-		gamepad4 = gamepad2;
-	}
-}
-
-class Toggle {
-	private boolean currentValue;
-	private boolean prevValue;
-	private boolean isToggled;
-	Toggle(boolean current) {
-		currentValue = current;
-		prevValue = current;
-	}
-	public void updateToggle(boolean newValue) {
-		prevValue = currentValue;
-		currentValue = newValue;
-		if(prevValue != currentValue) {
-			isToggled = !isToggled;
+	public boolean toggleButton(String name, boolean button) {
+		int index = 0;
+		for(int i = 0; i < names.length; i++) {
+			if(names[i].equals(name)) {
+				index = i;
+			}
 		}
-	}
-	public boolean getState() {
-		return isToggled;
+		boolean prevValue = buttons[index];
+		buttons[index] = button;
+		return button && ! buttons[index];
 	}
 }
