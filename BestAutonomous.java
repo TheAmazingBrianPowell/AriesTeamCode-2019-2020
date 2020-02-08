7d4fd294b963d7d391ffc886228f85ad74681e68
Brian Powell <47094532+TheAmazingBrianPowell@users.noreply.github.com>
2020-02-08 15:04:28 -0800

    Update BestAutonomous.java

--- BestAutonomous.java
+++ BestAutonomous.java
@@ -9,74 +9,78 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 public class BestAutonomous extends Bendy {
 	int skyPosition = 0;
 	
+	
+	
 	@Override
 	public void runOpMode() {
 			//sets up the robot
 			setUp(); 
+			telemetry.addData("status", "callibrating gyrosensor");
+			telemetry.update();
 			gyroCalibrate();
+			telemetry.addData("status", "initiallizing vuforia");
+			telemetry.update();
 			vuforiaInit();
 			telemetry.addData("status", "ready");
 			telemetry.update();
 		
 			waitForStart();
 			
-			drive(0,-0.75,0,400);
-			encoderStop();
-			//gyroAlign(0);
-			drive(0.75,0,0,1400);
-			encoderStop();
+			drive(0,-1,0,700);
+			gyroAlign(0);
+			drive(1,0,0,1400);
 			sleep(500);
 			if(!vuforiaSkystone()) {
-				drive(0,0.75,0,600);
-				sleep(500);
+				drive(0,1,0,500);
 				skyPosition = 1;
+				sleep(500);
 				if(!vuforiaSkystone()) {
-					drive(0,0.75,0,600);
+					drive(0,1,0,500);
 					skyPosition = 2;
 				}
 			}
-			encoderStop();
 			
-			//gyroAlign(0);
+			gyroAlign(0);
 			
-			drive(-0.75,0,0,400);
+			drive(-1,0,0,400);
 			setPosition("rotate", 0);
 			setPosition("rotate2", 1);
 			sleep(200);
 			clamp();
+			move("lift2", -1, 50);
+			move("lift", 1, 50);
+			encoderStop("lift");
+			encoderStop("lift2");
 			sleep(1800);
 			setPosition("rotate", 0.5);
 			setPosition("rotate2", 0.5);
-			encoderStop();
 			
-			//gyroAlign(0);
 			
-			drive(0.75,0,0,700);
-			encoderStop();
-			drive(0.75,0,0,300);
+			
+			gyroAlign(0);
+			
+			drive(1,0,0,700);
 			clamp();
-			encoderStop();
+			drive(1,0,0,300);
 			
 			
-			drive(-0.75,0,0,700);
-			encoderStop();
+			drive(-1,0,0,700);
 			//move("lift", 1, 100);
 			//move("lift2", 1, 100);
 			//encoderStop("lift");
 			//gyroAlign(0);
-			drive(0,-0.5,0);
+			drive(0,0.5,0);
 			
 			while(groundColor.blue() < 35 && opModeIsActive()) idle();
 			gyroAlign(0);
-			drive(0,0.75,0,3000);
-			encoderStop();
+			drive(0,-1,0,3000);
 			
 			move("lift2", -1, 100);
 			move("lift", 1, 100);
 			encoderStop("lift");
 			encoderStop("lift2");
 			
-			drive(0.75,0,0,300);
+			drive(1,0,0,300);
 			
 			clamp();
 			
